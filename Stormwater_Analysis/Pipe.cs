4e4a97a668f95171e7dea8634768fa75e915b571
milenna <milennadsouza@yahoo.com>
2018-10-09 12:23:15 -0700

    Initial Commit

---
+++ Stormwater_Analysis/Pipe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stormwater_Analysis
+{
+    class Pipe
+    {
+        
+
+        #region properties
+        public string Pipe_ID { get; set; }
+        public decimal Size { get; set; }
+        public string Dimensions { get; set; }
+        public string Material { get; set; }
+        public string Road_Crossing { get; set; }
+        public string Soil_Type { get; set; }
+        public decimal Ground_Slope { get; set; }
+        public bool Railway_Crossing { get; set; }
+        public string Critical_Infrastructure { get; set; }
+        public string Critical_Structure { get; set; }
+        public int Maintenance_Records_ID { get; set; }
+        #endregion
+
+        #region Constructor
+        
+
+
+
+        #endregion
+
+
+    }
+
+    class Manhole
+    {
+        #region properties
+        public int ManholeID { get; set; }
+        public int MyProperty { get; set; }
+        #endregion
+
+    }
+}
---
+++ Stormwater_Analysis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Stormwater_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var new_pipe = new Pipe();
+            
+            var new_pipe2 = new Pipe();
+
+            Console.WriteLine($"Pipe Number 1:{new_pipe.Pipe_ID}, Pipe Number 2: {new_pipe2.Pipe_ID}");
+        }
+    }
+}
