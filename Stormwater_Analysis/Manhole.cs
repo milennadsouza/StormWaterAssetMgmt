fd7fa34f753388324685d9a099efb02035e58dec
milenna <milennadsouza@yahoo.com>
2018-10-12 11:58:08 -0700

    2nd commit

---
+++ Stormwater_Analysis/Manhole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stormwater_Analysis
+{
+   
+    class Manhole
+    {
+        // static private variable to generate unique manhole ID's.
+        private static int manhole_value = 0;
+
+        #region properties
+        public int ManholeID { get; set; }
+        public DateTime InstallationDt { get; set; }
+        public string ManagedBy { get; set; }
+        public string DrainageArea { get; set; }
+        #endregion
+
+        #region Constructor
+        public Manhole()
+        {
+            manhole_value = manhole_value++;
+            ManholeID = manhole_value;
+        }
+        #endregion
+    }
+
+}
--- Stormwater_Analysis/Program.cs
+++ Stormwater_Analysis/Program.cs
@@ -6,11 +6,28 @@ namespace Stormwater_Analysis
     {
         static void Main(string[] args)
         {
-            var new_pipe = new Pipe();
-            
+            var new_pipe = new Pipe //create a new object initialization
+            {
+                Pipe_Type = TypesOfPipes.Mains, // set it to one of the enum type values.
+                DateOfInstallation = DateTime.Now,
+                Material = TypesOfMaterials.Concrete, // set it to one of the enum type values.
+                Depth =  20,
+                Critical_Infrastructure = true,
+                Critical_Structure = false,
+                Pipe_Length = 500,
+               
+                
+            };
+            new_pipe.Est_Life = new_pipe.PipeLife(new_pipe.Material);
+
+
+
+
+
+
             var new_pipe2 = new Pipe();
 
-            Console.WriteLine($"Pipe Number 1:{new_pipe.Pipe_ID}, Pipe Number 2: {new_pipe2.Pipe_ID}");
+            Console.WriteLine($"Pipe Number 1:{new_pipe.Pipe_ID},  Type of Pipe : {new_pipe.Pipe_Type} , Estimated years : {new_pipe.Est_Life} / n Pipe Number 2: {new_pipe2.Pipe_ID}");
         }
-    }
+    } 
 }
